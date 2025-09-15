bfeb86b1065d9e9d9398df6989b397a2f1cd2729
Amaregouda <164073706+Amaregoud@users.noreply.github.com>
2025-09-15 22:41:36 +0530

    Add files via upload

--- firstnonrepeatingcharacter.py
+++ firstnonrepeatingcharacter.py
@@ -1,7 +1,21 @@
-from collections import counter
+# from collections import counter
+# s=input()
+# freq=counter(s)
+# for ch in s:
+#     if freq[ch]==1:
+#         print(f"Found {ch} ")
+#         break
+# else:
+#     print("Not found")
+
 s=input()
-freq=counter(s)
+freq={}
 for ch in s:
+    if ch in freq:
+        freq[ch]+=1 
+    else:
+        freq[ch]=1 
+for ch in freq:
     if freq[ch]==1:
         print(f"Found {ch} ")
         break
